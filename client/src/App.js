ca7ffabe0bbe99dc154b1129efdec1c2416db5bd
rarshonsky <rob@twine.com>
2019-08-21 14:43:13 -0700

    fixes modal size; adds color; adds github repo link

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Header, Divider, Form, Button, Segment } from 'semantic-ui-react'
 import TruffleContract from 'truffle-contract'
 import ReactTable from 'react-table'
 
+
 import getWeb3 from './utils/getWeb3'
 import KeyStoreContract from './contracts/KeyStore.json'
 
@@ -185,7 +186,10 @@ class App extends Component {
     return (
 
       <div id="main" className="">
-        <Header as='h1'>Decentralized PGP Key Store </Header>
+        <Header as='h1' className="name">Decentralized Key Store</Header>
+        <Header as='h3'>
+          <a href="https://github.com/rarshonsky/key_store_dapp"><svg width="22" height="22" class="octicon octicon-mark-github" viewBox="0 0 16 16" version="1.1" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg></a>
+        </Header>
         <Divider />
         <div>
           <Segment>
@@ -231,7 +235,7 @@ class App extends Component {
 
 
         <div id="searchResults" className="fade modal" role="dialog">
-          <div className="modal-dialog">
+          <div className="modal-dialog modal-lg">
             <div className="modal-content">
               <div className="modal-header">
                 <h4 className="modal-title">Search Results</h4>
